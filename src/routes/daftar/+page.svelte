0907224cc495d96842c02ea03440b6c9a10831d4
KaisarOrange <alif.ayodya@gmail.com>
2024-09-22 14:12:46 +0700

    commit

--- src/routes/daftar/+page.svelte
+++ src/routes/daftar/+page.svelte
@@ -105,10 +105,10 @@
 							required
 						>
 							<option value="" disabled selected>Pilih program</option>
-							<option value="Ground Handling">Ground Handling (Kerja)</option>
-							<option value="Ground Handling">Ground Handling (Magang)</option>
-							<option value="Pertanian">Pertanian (Kerja)</option>
-							<option value="Pertanian">Pertanian (Magang)</option>
+							<option value="Ground Handling (Kerja)">Ground Handling (Kerja)</option>
+							<option value="Ground Handling (Magang)">Ground Handling (Magang)</option>
+							<option value="Pertanian (Kerja)">Pertanian (Kerja)</option>
+							<option value="Pertanian (Magang)">Pertanian (Magang)</option>
 						</select>
 					</div>
 					<div class="flex flex-col">
